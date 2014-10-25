b7a498751a70773332129d9f0f939fa157564938
Vadim Poplavskiy <im@demetrodon.com>
2014-10-26 02:29:34 +0300

    fixed path and add method translate with srt file

--- lib/translator_srt/google_translate.rb
+++ lib/translator_srt/google_translate.rb
@@ -1,12 +1,20 @@
-require 'google_translate/result_parser'
-require 'google_translate/service'
+require 'translator_srt/google_translate/result_parser'
+require 'translator_srt/google_translate/service'
 
 module TranslatorSrt
   module GoogleTranslate
-    def self.t(from_lang, to_lang, text)
+    def self.translate_srt_file(from_lang, to_lang, path_srt_file)
+      srt_file_content = File.read(path_srt_file)
+      result = Service.new.translate(from_lang, to_lang, srt_file_content)
+
+      puts "Translated srt string"
+
+      ResultParser.new(result).to_s
+    end
+    def self.translate_srt_string(from_lang, to_lang, text)
       result = Service.new.translate(from_lang, to_lang, text)
 
-      puts "Translated srt"
+      puts "Translated srt string"
 
       ResultParser.new(result).to_s
     end
